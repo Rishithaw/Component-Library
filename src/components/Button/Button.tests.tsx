7c2c1fea26dc49b1b69c6346cb02ab1786b5551a
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-10-10 11:15:11 -0500

    Made button component

---
+++ src/components/Button/Button.tests.tsx
@@ -0,0 +1,14 @@
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom'
+import {Button} from './Button';
+
+test('renders Button with label', () => {
+  render(<Button label="Test Button" />);
+  expect(screen.getByText('Click Me')).toBeInTheDocument();
+});
+
+test('Button has grey background when disabled', () => {
+  render(<Button label="Click Me" disabled />);
+  const button = screen.getByText('Click Me');
+  expect(button).toHaveStyle('background-color: #ccc');
+});
